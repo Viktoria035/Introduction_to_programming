efb8db569ab4e6dda906b28b957e408de3e572a5
Viktoria035 <115097521+Viktoria035@users.noreply.github.com>
2022-10-16 21:34:48 +0300

    Create Task09_1.2.cpp

---
+++ SI/Sem.02/Pract.02/Task09_1.2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int num1 = 0;
+	int num2 = 0;
+	int num3 = 0;
+	cout << "Please enter three integer numbers: " << endl;
+	cin >> num1 >> num2 >> num3;
+	if (num1 < num2 && num1 < num3 && num2 < num3)
+	{
+		cout << "In ascending order, the numbers are arranged like this: " << num1 << " " << num2 << " " << num3 << endl;
+	}
+	else if (num1 < num2 && num1 < num3 && num3 < num2)
+	{
+		cout << "In ascending order, the numbers are arranged like this: " << num1 << " " << num3 << " " << num2 << endl;
+	}
+	else if (num2 < num1 && num2 < num3 && num1 < num3)
+	{
+		cout << " In ascending order, the numbers are arranged like this: " << num2 << " " << num1 << " " << num3 << endl;
+	}
+	else if (num2 < num1 && num2 < num3 && num3 < num1)
+	{
+		cout << "In ascending order, the numbers are arranged like this: " << num2 << " " << num3 << " " << num1 << endl;
+	}
+	else if (num3 < num2 && num3 < num1 && num2 < num1)
+	{
+		cout << "In ascending order, the numbers are arranged like this: " << num3 << " " << num2 << " " << num1 << endl;
+	}
+	else if (num3 < num2 && num3 < num1 && num1 < num2)
+	{
+		cout << "In ascending order, the numbers are arranged like this: " << num3 << " " << num1 << " " << num2 << endl;
+	}
+	return 0;
+}
