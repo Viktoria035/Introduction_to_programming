168bdf4e7cc037db11dfb4d2a7494874908dd482
Viktoria035 <115097521+Viktoria035@users.noreply.github.com>
2022-10-16 21:38:06 +0300

    Create Task10.cpp

---
+++ SI/Sem.02/Pract.02/Task10.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	double num1 = 0;
+	double num2 = 0;
+	cout << "Please enter two Real number: " << endl;
+	cin >> num1 >> num2;
+	cout << "Their integer division is: " << num1 / num2 << endl;
+	cout << "Their is sum is: " << num1 + num2 << endl;
+	cout << "Their difference: " << num1 - num2 << endl;
+	cout << "Their multiplication is: " << num1 * num2 << endl;
+	if (num2 = 0 && num1 / num2)
+	{
+		cout << "There is no answer." << endl;
+	}
+	return 0;
+}
