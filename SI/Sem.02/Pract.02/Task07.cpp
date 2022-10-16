948635a500e6e253f06822607477e514af92ab27
Viktoria035 <115097521+Viktoria035@users.noreply.github.com>
2022-10-16 21:24:23 +0300

    Create Task07.cpp

---
+++ SI/Sem.02/Pract.02/Task07.cpp
@@ -0,0 +1,226 @@
+﻿#include <iostream>
+int main()
+{
+	char symbol;
+	std::cout << "Please enter a letter from the Latin alphabet: ";
+	std :: cin >> symbol;
+	
+	switch (symbol)
+	{
+	case 'a':
+		symbol = 'A';
+		std::cout << "The opposite symbol now is " << symbol << std::endl;
+		break;
+	case 'b':
+		symbol = 'B';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'c':
+		symbol = 'C';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'd':
+		symbol = 'D';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'e':
+		symbol = 'E';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'f':
+		symbol = 'F';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'g':
+		symbol = 'G';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'h':
+		symbol = 'H';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'i':
+		symbol = 'I';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'j':
+		symbol = 'J';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'k':
+		symbol = 'K';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'l':
+		symbol = 'L';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'm':
+		symbol = 'M';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'n':
+		symbol = 'N';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'o':
+		symbol = 'O';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'p':
+		symbol = 'P';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'q':
+		symbol = 'Q';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'r':
+		symbol = 'R';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 's':
+		symbol = 'S';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 't':
+		symbol = 'T';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'u':
+		symbol = 'U';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'v':
+		symbol = 'V';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'w':
+		symbol = 'W';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'x':
+		symbol = 'X';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'y':
+		symbol = 'Y';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'z':
+		symbol = 'Z';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'A':
+		symbol = 'a';
+		std::cout << "The opposite symbol now is " << symbol << std::endl;
+		break;
+	case 'B':
+		symbol = 'b';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'C':
+		symbol = 'c';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'D':
+		symbol = 'd';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'E':
+		symbol = 'e';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'F':
+		symbol = 'f';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'G':
+		symbol = 'g';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'H':
+		symbol = 'h';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'I':
+		symbol = 'i';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'J':
+		symbol = 'j';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'K':
+		symbol = 'k';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'L':
+		symbol = 'l';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'M':
+		symbol = 'm';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'N':
+		symbol = 'n';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'O':
+		symbol = 'o';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'P':
+		symbol = 'p';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'Q':
+		symbol = 'q';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'R':
+		symbol = 'r';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'S':
+		symbol = 's';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'T':
+		symbol = 't';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'U':
+		symbol = 'u';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'V':
+		symbol = 'v';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'W':
+		symbol = 'w';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'X':
+		symbol = 'x';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'Y':
+		symbol = 'y';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	case 'Z':
+		symbol = 'z';
+		std::cout << "The opposite symbol is " << symbol << std::endl;
+		break;
+	default:
+		std::cout << "It is not a letter from the Latin alphabet." << std::endl;
+
+	
+	}
+	
+	//Да се напише програма, която чете от конзолата символ и, ако е малка буква, извежда съответната голяма, ако е голяма буква, извежда съответната малка, а ако не е буква, да изведе съобщение за невалиден символ.
+	return 0;
+}
