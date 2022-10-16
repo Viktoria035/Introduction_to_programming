dade82995e606d2bd163d95a86ba8068f0405e55
Viktoria035 <115097521+Viktoria035@users.noreply.github.com>
2022-10-16 21:19:56 +0300

    1

---
+++ SI/Sem.02/Pract.02/Task03.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	double  a = 0;
+	double b = 0;
+	cin >> a >> b;
+
+
+
+	a > b ? cout << "Number A is bigger than number B." : cout << "Number B is bigger than number A";
+
+
+
+
+	return 0;
+}
